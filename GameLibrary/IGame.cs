687168b0236495a145bb86d0cd2c0027637efd21
nicklozon <nicklozon@ac37567a-cef4-dca2-63f1-0c01d7e03cf1>
2010-03-22 18:37:51 +0000

    Initial Commit

---
+++ GameLibrary/IGame.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GameLibrary
+{
+    public interface IGame
+    {
+        // Members
+        int PlayerOneScore { get; }
+        int PlayerTwoScore { get; }
+        String PlayerTurn { get; }
+        List<Cell> RevealedCells { get; }
+
+        // Methods
+        void revealBomb();
+        void revealCell();
+    }
+}
